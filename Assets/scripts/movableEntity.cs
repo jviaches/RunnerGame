8d4060ea861148dde8554b46bbc7f8b06af8c271
Slava <jviaches@gmail.com>
2018-04-10 19:23:00 -0500

    Initial code

---
+++ Assets/scripts/movableEntity.cs
@@ -0,0 +1,81 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class movableEntity : MonoBehaviour
+{
+    private Rigidbody rb;
+    private float speed = 5f;
+
+    private int coinsAmount = 0;
+    public Text coinText;
+
+    private bool isMovingRight = true;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        coinText.text = "Coins: " + coinsAmount.ToString();
+    }
+
+    void changeDirection()
+    {
+        isMovingRight = !isMovingRight;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+            changeDirection();
+
+        if (isMovingRight)
+            rb.velocity = new Vector3(speed, rb.velocity.y > 0 ? 0: rb.velocity.y, 0f);
+        else
+            rb.velocity = new Vector3(0f, rb.velocity.y > 0 ? 0 : rb.velocity.y, speed);
+
+        //detectFreeFall();
+        detectBonus();
+
+        coinText.text = "Coins: " + coinsAmount.ToString();
+    }
+
+    private bool detectFreeFall()
+    {
+        RaycastHit hit;
+
+        if (Physics.Raycast(transform.position, Vector3.down, out hit, 5f) && hit.transform.gameObject.tag == "Ground")
+            return false;
+        else
+            return true;
+    }
+
+    private void detectBonus()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(transform.position, Vector3.forward, out hit, 1f) && hit.transform.gameObject.tag == "coin")
+        {
+            coinsAmount++;
+            Destroy(hit.transform.gameObject);
+        }
+
+        if (Physics.Raycast(transform.position, Vector3.back, out hit, 1f) && hit.transform.gameObject.tag == "coin")
+        {
+            coinsAmount++;
+            Destroy(hit.transform.gameObject);
+        }
+
+        if (Physics.Raycast(transform.position, Vector3.right, out hit, 1f) && hit.transform.gameObject.tag == "coin")
+        {
+            coinsAmount++;
+            Destroy(hit.transform.gameObject);
+        }
+
+        if (Physics.Raycast(transform.position, Vector3.left, out hit, 1f) && hit.transform.gameObject.tag == "coin")
+        {
+            coinsAmount++;
+            Destroy(hit.transform.gameObject);
+        }
+    }
+}
