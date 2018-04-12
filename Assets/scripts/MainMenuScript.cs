a436b30fec4873af8d8e5ae525717db38060f5f8
Slava <jviaches@gmail.com>
2018-04-12 16:59:57 -0500

    Wired exit button from main menu

--- Assets/scripts/MainMenuScript.cs
+++ Assets/scripts/MainMenuScript.cs
@@ -8,17 +8,26 @@ using UnityEngine.UI;
 public class MainMenuScript : MonoBehaviour {
 
     public Button StartMenuBtnButton;               // Start Game button in main menu
+    public Button ExitMenuBtnButton;               // Start Game button in main menu
 
     void Start () {
 
-        Button btn = StartMenuBtnButton.GetComponent<Button>();
-        btn.onClick.AddListener(StartMenuButtonClick);
+        StartMenuBtnButton.GetComponent<Button>().onClick.AddListener(StartMenuButtonClick);
+        ExitMenuBtnButton.GetComponent<Button>().onClick.AddListener(ExitMenuButtonClick);
     }
 
     private void StartMenuButtonClick()
     {
         SceneManager.LoadScene("developmentScene");
         Debug.Log("StartMenuButtonClick()");
+    }
+
+    private void ExitMenuButtonClick()
+    {
+        // Quit is ignored in the editor
+        // https://docs.unity3d.com/ScriptReference/Application.Quit.html for reference
+        Application.Quit();
+        Debug.Log("ExitMenuButtonClick()");
     }
 
     // Update is called once per frame
