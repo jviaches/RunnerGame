f17aba5278ddd457c1f2a78575c3513a4f196a3c
jstreamer <streamer.android@gmail.com>
2018-04-25 14:59:34 +0300

    Added model movement for inspection

--- Assets/scripts/try1/PlayerControllerScript.cs
+++ Assets/scripts/try1/PlayerControllerScript.cs
@@ -4,17 +4,43 @@ using UnityEngine;
 
 public class PlayerControllerScript : MonoBehaviour {
 
-	public float playerSpeen = 0.5f;//tiles of road per second
+	public float EngineForce = 200f;
+	public float SteeringForce = 65f;
+	public float BreakForce = 1f;
 	public bool IsMoving = false;
+	public WheelCollider RighFrontW;
+	public WheelCollider RighBacktW;
+	public WheelCollider LeftFrontW;
+	public WheelCollider LeftBackW;
+
 	public Vector3 movementDirection;
+	public GameObject playerObject;
+
+	private Rigidbody playersBody;
+	private float _moveZ;
 
 
 	void Start () {
-		
+
+
+		movementDirection = new Vector3 (1, 0, 0);
+
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+		float v = Input.GetAxis ("Vertical") * EngineForce;
+		float h = Input.GetAxis ("Horizontal") * SteeringForce;
+
+		RighBacktW.motorTorque = v;
+		LeftBackW.motorTorque = v;
+
+		RighFrontW.steerAngle = h;
+		LeftFrontW.steerAngle = h;
+
+		//RighFrontW.motorTorque = v;
+		//LeftFrontW.motorTorque = v;
 	}
+
 }
