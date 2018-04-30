8b5f527c9f50c350d5764aa2861f4c144fba938d
jstreamer <streamer.android@gmail.com>
2018-04-30 17:02:31 +0300

    Negative Y detection

--- Assets/scripts/try1/PlayerControllerScript.cs
+++ Assets/scripts/try1/PlayerControllerScript.cs
@@ -19,11 +19,13 @@ public class PlayerControllerScript : MonoBehaviour {
 	private Rigidbody playersBody;
 	private float _moveZ;
 
+
+	private LevelManagerScript lmScript;
 	public bool GameOver = false;
 
 	void Start () {
 
-
+		lmScript =  GameObject.Find("LevelManager").GetComponent<LevelManagerScript>();
 		movementDirection = new Vector3 (1, 0, 0);
 
 
@@ -31,6 +33,9 @@ public class PlayerControllerScript : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		if (GameObject.Find("PlayerBody").transform.position.y < -1) {
+			lmScript.FinishLevel (true);
+		}
 		if (!GameOver) {
 			float v = Input.GetAxis ("Vertical") * EngineForce;
 			float h = Input.GetAxis ("Horizontal") * SteeringForce;
