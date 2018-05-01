031c9a39c15ee032831d19afc0fcf01729e54c86
Viacheslav Blinder <jviaches@gmail.com>
2018-05-01 00:00:12 -0500

    Adopting all scripts and relations between them

--- Assets/scripts/LevelManager/LevelManagerScript.cs
+++ Assets/scripts/LevelManager/LevelManagerScript.cs
@@ -1,84 +1,172 @@
-﻿using System.Collections;
+﻿using Assets.scripts.DialogModule;
+using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class LevelManagerScript : MonoBehaviour {
+public class LevelManagerScript : MonoBehaviour
+{
+    public GameObject RoadManager;
+    public float roadTileCreationSpeed = 1;     // tile per second
+    public int directionChangingFrequency = 3;  // Random 0 =  change direction
 
-	public GameObject RoadManager;
-	public int directionChangingFrequency =3; // Random 0 =  change direction
-	public float droneRedrawSpeed = 60f;
-	public GameObject Drone;
-	private bool GameOver = false;
-	public PlayerControllerScript playerScript;
-	public GameObject GameOverPanel;
-	public Text EndLevelMessageText;
+    public float droneRedrawSpeed = 60f;
+    public GameObject Drone;
 
-	private Timer timerScript;
+    public PlayerControllerScript playerScript;
 
-	public void FinishLevel(bool playerLost){
-		if (playerLost) {
-			GameOver = true;
-			playerScript.GameOver = true;
-			GameOverPanel.SetActive (true);
-			Debug.Log ("Player has lost. GameOver.");
-		} else {
-			GameOver = true;
-			playerScript.GameOver = true;
-			EndLevelMessageText.text = "Level Completed !";
-			GameOverPanel.SetActive (true);
-			Debug.Log ("Player has Won. GameOver.");
-		}
-	}
+    public int Level = 1;
+    public int timeToSurvive = 60; //in seconds
 
+    public GameObject MainModalPanel;               // ..
+    public GameObject SuccessModalPanel;            // Modal Dialog when user successfully completed level
+    public Text SuccessModalTitle;                  // Caption of modal dialog
+    public Button SuccessModalOkButton;             // OK button in Success modal dialog
+    public Button SuccessModalNextLvlButton;        // Next level button in Success modal dialog
 
-	public float roadTileCreationSpeed=1;//tile per second
+    public GameObject FailModalPanel;               // Modal Dialog when user Fail to complete level
+    public Text FailModalTitle;                     // Caption of modal dialog
+    public Button FailModalOkButton;                // OK button in Fail modal dialog
+    public Button FailModalRepeatLvlButton;         // repeat level button in Fail modal dialog
 
+    private DialogManager dialogManager;
 
-	private RoadGenerationScript roadScript;
+    private bool GameOver = false;
+    private Timer timerScript;
+    private RoadGenerationScript roadScript;
 
+    void Start()
+    {
+        InitAllPanelsAndDialogs();
 
-	void Start () {
-		GameOverPanel.SetActive (false);
-		timerScript =this.GetComponent<Timer>();
-		roadScript = (RoadGenerationScript)RoadManager.GetComponent<RoadGenerationScript> ();
-		if(roadScript==null){
-			Debug.Log ("Failed to load roads script. Exiting");
-			return;
-		}
-		timerScript.StartTimer ();
-		AdvancingRoad ();
-		DroneMovement ();
+        timeToSurvive = Level * 60;
 
-	}
-	private void DroneMovement (){
-		Vector3 currentLocation = Drone.transform.position;
-		Drone.transform.position = currentLocation + (roadScript.frontWall.transform.position+ new Vector3(0,5,0) - currentLocation)*roadTileCreationSpeed  / droneRedrawSpeed ;
-		Drone.transform.Rotate (roadScript.newTileRotation*roadTileCreationSpeed  / droneRedrawSpeed );
-		Invoke ("DroneMovement", 1 / droneRedrawSpeed);
-	}
+        timerScript = this.GetComponent<Timer>();
+        roadScript = RoadManager.GetComponent<RoadGenerationScript>();
 
-	private void AdvancingRoad(){
-		if (!GameOver) {
-			roadScript.ForceStart ();
-			roadScript.AdvanceRoad ();
-			Invoke ("AdvancingRoad", 1 / roadTileCreationSpeed);
-		}
-	}
+        if (roadScript == null)
+        {
+            Debug.Log("Failed to load roads script. Exiting");
+            return;
+        }
 
-	/*
+        timerScript.StartTimer();
+        AdvancingRoad();
+        DroneMovement();
+    }
+
+    private void DroneMovement()
+    {
+        Vector3 currentLocation = Drone.transform.position;
+
+        Drone.transform.position = currentLocation + 
+                                   (roadScript.frontWall.transform.position + new Vector3(0, 5, 0) - currentLocation) * 
+                                   roadTileCreationSpeed / droneRedrawSpeed;
+
+        Drone.transform.Rotate(roadScript.newTileRotation * roadTileCreationSpeed / droneRedrawSpeed);
+
+        Invoke("DroneMovement", 1 / droneRedrawSpeed);
+    }
+
+    private void AdvancingRoad()
+    {
+        if (!GameOver)
+        {
+            roadScript.ForceStart();
+            roadScript.AdvanceRoad();
+
+            Invoke("AdvancingRoad", 1 / roadTileCreationSpeed);
+        }
+    }
+
+    public void FinishLevel(bool playerLost)
+    {
+        GameOver = true;
+        playerScript.GameOver = true;
+        //GameOverPanel.SetActive(true);
+
+        if (playerLost)
+            showModalFailLevelDialog();
+        else
+            showModalSuccessLevelCompletedDialog();      
+    }
+
+    /*
 		calculating if next tile should be an obsticle
 		for now we are using simple Random, but could use complex function 
 		to determine complexity level
 	*/
-	private bool IsNextTileAnObsticle()
-	{
-		return Random.Range (0, directionChangingFrequency + 1) == 0;
-	}
+    private bool IsNextTileAnObsticle()
+    {
+        return UnityEngine.Random.Range(0, directionChangingFrequency + 1) == 0;
+    }
 
+    private void InitAllPanelsAndDialogs()
+    {
+        dialogManager = new DialogManager();
 
+        Dictionary<Button, UnityAction> successModalDictionary = new Dictionary<Button, UnityAction>();
+        successModalDictionary.Add(SuccessModalNextLvlButton, loadNextLevelModalDialog);
+        successModalDictionary.Add(SuccessModalOkButton, loadMainMenuModalDialog);
 
+        ModalDialog successModalDialog = new ModalDialog(SuccessModalTitle, SuccessModalPanel, successModalDictionary, MainModalPanel);
+        dialogManager.AddDialog(successModalDialog);
 
-	}
+        Dictionary<Button, UnityAction> failModalDictionary = new Dictionary<Button, UnityAction>();
+        failModalDictionary.Add(FailModalRepeatLvlButton, repeatLevelFailModalDialog);
+        failModalDictionary.Add(FailModalOkButton, loadMainMenuModalDialog);
 
+        ModalDialog failModalDialog = new ModalDialog(FailModalTitle, FailModalPanel, failModalDictionary, MainModalPanel);
+        dialogManager.AddDialog(failModalDialog);
 
+        dialogManager.CloseAllOpenedModalDialogs();
+    }
+
+    private void loadNextLevelModalDialog()
+    {
+        dialogManager.CloseAllOpenedModalDialogs();
+
+        //startLevel();
+    }
+
+    private void repeatLevelFailModalDialog()
+    {
+        dialogManager.CloseAllOpenedModalDialogs();
+
+        //startLevel();
+    }
+
+    private void loadMainMenuModalDialog()
+    {
+        SceneManager.LoadScene(0);
+    }
+
+    private void showModalSuccessLevelCompletedDialog()
+    {
+        GameOver = false;
+        CancelInvoke("spawnPlatform");
+
+        //playerScript.SetSpeed(0);
+
+        SuccessModalPanel.transform.position = MainModalPanel.transform.position;
+        SuccessModalPanel.SetActive(true);
+        SuccessModalTitle.text = "Level " + Level + " completed !";
+
+        Level++;
+    }
+
+    private void showModalFailLevelDialog()
+    {
+        GameOver = false;
+        CancelInvoke("spawnPlatform");
+
+        //playerScript.SetSpeed(0);
+        //playerScript.SetMovingDirection(true);
+
+        dialogManager.ShowModalDialog(FailModalPanel, MainModalPanel);
+        FailModalTitle.text = "Level " + Level + " Failed !";
+    }
+}
--- Assets/scripts/Player/PlayerControllerScript.cs
+++ Assets/scripts/Player/PlayerControllerScript.cs
@@ -2,63 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerControllerScript : MonoBehaviour {
+public class PlayerControllerScript : MonoBehaviour
+{
+    public float EngineForce = 200f;
+    public float SteeringForce = 65f;
+    public float BreakForce = 1f;
 
-	public float EngineForce = 200f;
-	public float SteeringForce = 65f;
-	public float BreakForce = 1f;
-	public bool IsMoving = false;
-	public WheelCollider RighFrontW;
-	public WheelCollider RighBacktW;
-	public WheelCollider LeftFrontW;
-	public WheelCollider LeftBackW;
+    public bool IsMoving = false;
 
-	public Vector3 movementDirection;
-	public GameObject playerObject;
+    public WheelCollider RighFrontW;
+    public WheelCollider RighBacktW;
+    public WheelCollider LeftFrontW;
+    public WheelCollider LeftBackW;
 
-	private Rigidbody playersBody;
-	private float _moveZ;
+    public Vector3 movementDirection;
+    public GameObject playerObject;
 
+    public bool GameOver = false;
 
-	private LevelManagerScript lmScript;
-	public bool GameOver = false;
+    private Rigidbody playersBody;
+    private float _moveZ;
+    private LevelManagerScript levelManagerScript;
+   
 
-	void Start () {
+    void Start()
+    {
+        levelManagerScript = GameObject.Find("LevelManager").GetComponent<LevelManagerScript>();
+        movementDirection = new Vector3(1, 0, 0);
+    }
 
-		lmScript =  GameObject.Find("LevelManager").GetComponent<LevelManagerScript>();
-		movementDirection = new Vector3 (1, 0, 0);
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameObject.Find("PlayerBody").transform.position.y < -1)
+            levelManagerScript.FinishLevel(true);
 
+        if (!GameOver)
+        {
+            float v = Input.GetAxis("Vertical") * EngineForce;
+            float h = Input.GetAxis("Horizontal") * SteeringForce;
 
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		if (GameObject.Find("PlayerBody").transform.position.y < -1) {
-			lmScript.FinishLevel (true);
-		}
-		if (!GameOver) {
-			float v = Input.GetAxis ("Vertical") * EngineForce;
-			float h = Input.GetAxis ("Horizontal") * SteeringForce;
+            RighBacktW.motorTorque = v;
+            LeftBackW.motorTorque = v;
 
-			RighBacktW.motorTorque = v;
-			LeftBackW.motorTorque = v;
+            RighFrontW.steerAngle = h;
+            LeftFrontW.steerAngle = h;
+        }
+        else
+        {
+            RighBacktW.brakeTorque = Mathf.Infinity;
+            LeftBackW.brakeTorque = Mathf.Infinity;
 
-			RighFrontW.steerAngle = h;
-			LeftFrontW.steerAngle = h;
-
-			//RighFrontW.motorTorque = v;
-			//LeftFrontW.motorTorque = v;
-
-		} else {
-			RighBacktW.brakeTorque=Mathf.Infinity;
-			LeftBackW.brakeTorque=Mathf.Infinity;
-
-			GameObject.Find ("PlayerBody").GetComponent<Rigidbody> ().isKinematic = true;
-
-		}
-	}
-
-
-
-
+            GameObject.Find("PlayerBody").GetComponent<Rigidbody>().isKinematic = true;
+        }
+    }
 }
