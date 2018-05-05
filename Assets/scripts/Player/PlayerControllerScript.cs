3794de6585e81802f267df74b4b5d297f059eaad
Viacheslav Blinder <jviaches@gmail.com>
2018-05-04 21:01:21 -0500

    small indentation and UI

--- Assets/scripts/Player/PlayerControllerScript.cs
+++ Assets/scripts/Player/PlayerControllerScript.cs
@@ -15,35 +15,32 @@ public class PlayerControllerScript : MonoBehaviour
     public WheelCollider LeftFrontW;
     public WheelCollider LeftBackW;
 
-	public Transform RighFrontWTransform;
-	public Transform RighBacktWTransform;
-	public Transform LeftFrontWTransform;
-	public Transform LeftBackWTransform;
+    public Transform RighFrontWTransform;
+    public Transform RighBacktWTransform;
+    public Transform LeftFrontWTransform;
+    public Transform LeftBackWTransform;
 
     public Vector3 movementDirection;
-	private Rigidbody carRigidBody;
+    private Rigidbody carRigidBody;
 
-	public int WheelRoattioVisualModifyer= 25
-		;
-	public Text speedometer;
-	public float maxSpeed=10f;
+    public int WheelRoattioVisualModifyer = 25;
+    public Text speedometer;
+    public float maxSpeed = 10f;
 
     public bool GameOver = false;
 
-
-	 
     private LevelManagerScript levelManagerScript;
-   
-	private float ScreenWidth;
-	private float currentTurn=0f;
+
+    private float ScreenWidth;
+    private float currentTurn = 0f;
 
     void Start()
     {
-		
-		carRigidBody = GameObject.Find ("Buggy").GetComponent<Rigidbody> ();
+
+        carRigidBody = GameObject.Find("Buggy").GetComponent<Rigidbody>();
         levelManagerScript = GameObject.Find("LevelManager").GetComponent<LevelManagerScript>();
         movementDirection = new Vector3(1, 0, 0);
-		ScreenWidth = Screen.width;
+        ScreenWidth = Screen.width;
     }
 
     // Update is called once per frame
@@ -52,45 +49,49 @@ public class PlayerControllerScript : MonoBehaviour
         if (GameObject.Find("Buggy").transform.position.y < -1)
             levelManagerScript.FinishLevel(true);
 
-		if (!GameOver) {
-			//float v = Input.GetAxis("Vertical") * EngineForce;
+        if (!GameOver)
+        {
+            //float v = Input.GetAxis("Vertical") * EngineForce;
 
-			if (Application.platform == RuntimePlatform.Android) {
-				currentTurn = CrossPlatformInputManager.GetAxis ("Horizontal") * SteeringForce;
-			}
+            if (Application.platform == RuntimePlatform.Android)
+            {
+                currentTurn = CrossPlatformInputManager.GetAxis("Horizontal") * SteeringForce;
+            }
 
-			if (Application.platform == RuntimePlatform.WindowsEditor){
-				currentTurn = Input.GetAxis ("Horizontal") * SteeringForce;
-			}
+            if (Application.platform == RuntimePlatform.WindowsEditor)
+            {
+                currentTurn = Input.GetAxis("Horizontal") * SteeringForce;
+            }
 
-	
-			RighFrontW.steerAngle = currentTurn;
-			LeftFrontW.steerAngle = currentTurn;
+            RighFrontW.steerAngle = currentTurn;
+            LeftFrontW.steerAngle = currentTurn;
 
+            RighBacktWTransform.Rotate(RighBacktW.rpm / WheelRoattioVisualModifyer / 60 * 360 * Time.deltaTime, 0, 0);
+            RighFrontWTransform.Rotate(RighFrontW.rpm / WheelRoattioVisualModifyer / 60 * 360 * Time.deltaTime, 0, 0);
+            LeftFrontWTransform.Rotate(LeftFrontW.rpm / WheelRoattioVisualModifyer / 60 * 360 * Time.deltaTime, 0, 0);
+            LeftBackWTransform.Rotate(LeftBackW.rpm / WheelRoattioVisualModifyer / 60 * 360 * Time.deltaTime, 0, 0);
 
-			RighBacktWTransform.Rotate (RighBacktW.rpm/WheelRoattioVisualModifyer / 60  * 360 * Time.deltaTime, 0, 0);
-			RighFrontWTransform.Rotate (RighFrontW.rpm/WheelRoattioVisualModifyer / 60 * 360 * Time.deltaTime, 0, 0);
-			LeftFrontWTransform.Rotate (LeftFrontW.rpm/WheelRoattioVisualModifyer / 60 * 360 * Time.deltaTime, 0, 0);
-			LeftBackWTransform.Rotate (LeftBackW.rpm/WheelRoattioVisualModifyer  / 60  * 360 * Time.deltaTime, 0, 0);
+            float currentspeed = carRigidBody.velocity.magnitude;
 
-			float currentspeed = carRigidBody.velocity.magnitude;
+            //2.0f * 3.14f * RighBacktW.radius * RighBacktW.rpm * 60 + "";
 
-				//2.0f * 3.14f * RighBacktW.radius * RighBacktW.rpm * 60 + "";
+            if (currentspeed < maxSpeed)
+            {
+                RighBacktW.motorTorque = EngineForce * (maxSpeed - currentspeed) / maxSpeed;
+                RighBacktW.brakeTorque = 0;
+                LeftBackW.motorTorque = EngineForce * (maxSpeed - currentspeed) / maxSpeed;
+                LeftBackW.brakeTorque = 0;
+            }
+            else if (currentspeed > maxSpeed)
+            {
+                RighBacktW.brakeTorque = EngineForce * BreakForce;
+                LeftBackW.brakeTorque = EngineForce * BreakForce;
+            }
 
-			if (currentspeed < maxSpeed) {
-				RighBacktW.motorTorque = EngineForce*(maxSpeed-currentspeed)/maxSpeed;
-				RighBacktW.brakeTorque = 0;
-				LeftBackW.motorTorque = EngineForce*(maxSpeed-currentspeed)/maxSpeed;
-				LeftBackW.brakeTorque = 0;
-			} else if (currentspeed > maxSpeed) {
-				RighBacktW.brakeTorque = EngineForce*BreakForce;
-				LeftBackW.brakeTorque = EngineForce*BreakForce;
-			}
+            speedometer.text = "SPEED: " + Mathf.RoundToInt(currentspeed);
 
-			speedometer.text = "SPEED: "+Mathf.RoundToInt (currentspeed);
-
-			RighFrontW.steerAngle = currentTurn;
-			LeftFrontW.steerAngle = currentTurn;
+            RighFrontW.steerAngle = currentTurn;
+            LeftFrontW.steerAngle = currentTurn;
         }
         else
         {
