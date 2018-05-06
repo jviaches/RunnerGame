8f3b5764637fb11b54ee961a0f63d8ec5b230e1d
Viacheslav Blinder <jviaches@gmail.com>
2018-05-06 15:42:57 -0500

    Hide level arrows (directions for buggy) upon dialog loading

--- Assets/scripts/LevelManager/LevelManagerScript.cs
+++ Assets/scripts/LevelManager/LevelManagerScript.cs
@@ -55,6 +55,7 @@ public class LevelManagerScript : MonoBehaviour
             return;
         }
 
+        SetVisualOfCanvasButtons(true);
         AdvancingRoad();
         DroneMovement();
     }
@@ -117,87 +118,24 @@ public class LevelManagerScript : MonoBehaviour
         GameOver = true;
         playerScript.GameOver = true;
 
+        SetVisualOfCanvasButtons(false);
+
         if (playerLost)
             showModalFailLevelDialog();
         else
-            showModalSuccessLevelCompletedDialog();      
+            showModalSuccessLevelCompletedDialog();
     }
 
-    /*
-=======
-public class LevelManagerScript : MonoBehaviour {
+    private void SetVisualOfCanvasButtons(bool status)
+    {
+        Button[] canvasButtons = MainModalPanel.GetComponentsInChildren<Button>();
+        if (canvasButtons != null && canvasButtons.Length != 0)
+        {
+            foreach (Button btn in canvasButtons)
+                btn.gameObject.SetActive(status);
+        }
+    }
 
-	public GameObject RoadManager;
-	public int directionChangingFrequency =3; // Random 0 =  change direction
-	public float droneRedrawSpeed = 60f;
-	public GameObject Drone;
-	private bool GameOver = false;
-	public PlayerControllerScript playerScript;
-	public GameObject GameOverPanel;
-	public Text EndLevelMessageText;
-
-	private Timer timerScript;
-
-	public void FinishLevel(bool playerLost){
-		if (playerLost) {
-			
-		
-
-			Debug.Log ("Player has lost. GameOver.");
-		} else {
-			
-
-			EndLevelMessageText.text = "Level Completed !";
-
-			Debug.Log ("Player has Won. GameOver.");
-		}
-		roadScript.frontWall.GetComponent<FrontWallAnimation> ().StopAnimation ();
-		playerScript.GameOver = true;
-		GameOverPanel.SetActive (true);
-		GameOver = true;
-	}
-
-
-	public float roadTileCreationSpeed=1;//tile per second
-
-
-	private RoadGenerationScript roadScript;
-
-
-	void Start () {
-		GameOverPanel.SetActive (false);
-		timerScript =this.GetComponent<Timer>();
-		roadScript = (RoadGenerationScript)RoadManager.GetComponent<RoadGenerationScript> ();
-		if(roadScript==null){
-			Debug.Log ("Failed to load roads script. Exiting");
-			return;
-		}
-		timerScript.StartTimer ();
-		AdvancingRoad ();
-		DroneMovement ();
-
-	}
-	private void DroneMovement (){
-		Vector3 currentLocation = Drone.transform.position;
-		Drone.transform.position = currentLocation + (roadScript.frontWall.transform.position+ new Vector3(0,5,0) - currentLocation)*roadTileCreationSpeed  / droneRedrawSpeed ;
-		Drone.transform.Rotate (roadScript.newTileRotation*roadTileCreationSpeed  / droneRedrawSpeed );
-		Invoke ("DroneMovement", 1 / droneRedrawSpeed);
-	}
-
-	private void AdvancingRoad(){
-		if (!GameOver) {
-			roadScript.ForceStart ();
-			roadScript.AdvanceRoad ();
-			Invoke ("AdvancingRoad", 1 / roadTileCreationSpeed);
-		}
-	}
-
-	/*
->>>>>>> RoadRework
-		calculating if next tile should be an obsticle
-		for now we are using simple Random, but could use complex function 
-		to determine complexity level
-	*/
     private bool IsNextTileAnObsticle()
     {
         return UnityEngine.Random.Range(0, directionChangingFrequency + 1) == 0;
